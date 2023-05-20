1a75d086c083f1829e1201d2c446939b6acff6ad
Naruewada <naruewada.115244@mail.kmutt.ac.th>
2023-05-20 21:15:07 +0700

    fix api

--- routes/style_routes.js
+++ routes/style_routes.js
@@ -1,67 +1,82 @@
 var router = require('express').Router()
 const { db } = require('../firebase')
 const middleware = require('../middleware')
-const admin = require('firebase-admin')
 
-router.post('/createFashionData', middleware.checkToken, async (req,res)=>{
-    try{
-        console.log(req.body)
-        const fashionCollection = db.collection('fashion')
-        await fashionCollection.add(req.body)
-        res.status(200).json({
-            msg:'success'
-        })
-    }catch(error){
-        console.log(error)
-        res.send(error)
-    }
+router.get('/:item_id', async (req, res) => {
+  try {
+    const { item_id } = req.params
+    let result = {}
+
+    const getFashionCollection = await db.collection('fashion').doc(item_id).get()
+    result = getFashionCollection.data()
+
+    res.status(200).json(result)
+  } catch (error) {
+    console.log(error)
+    res.send(error)
+  }
 })
 
-router.get('/showFashionData', async(req,res)=>{
-    try {
-        let allFashionData = []
-        const getFashionCollection = await db.collection('fashion').get()
-        getFashionCollection.forEach(val => {
-            // console.log(val.data())
-            allFashionData.push(val.data())
-        });
-        console.log(getFashionCollection)
-        res.status(200).json({
-            msg:'success',
-            getAllData:allFashionData
-        })
-    } catch (error) {
-        console.log(error)
-        res.send(error)
-    }
-})
+router.get('/', async (req, res) => {
+  try {
+    const { type } = req.query
+    let result = []
 
-router.patch('/:fashionID', async(req,res)=>{
-    try {
-        const {fashionID} = req.params
-        console.log(fashionID)
-        const updateFashionData = await db.collection('fashion').doc(fashionID).update({...req.body})
-        res.status(200).json({
-            msg:'success',
+    const getFashionCollection = await db.collection('fashion').where('category', '==', type).get()
+    getFashionCollection.forEach((val) => {
+      result.push(val.data())
     })
-    } catch (error) {
-        console.log(error)
-        res.send(error)
-    }
+
+    res.status(200).json({ data: result })
+  } catch (error) {
+    console.log(error)
+    res.send(error)
+  }
 })
 
-router.delete('/:fashionID', async(req,res)=>{
-    try {
-        const {fashionID} = req.params
-        console.log(fashionID)
-        const updateFashionData = await db.collection('fashion').doc(fashionID).delete()
-        res.status(200).json({
-            msg:'success',
+router.post('/createFashionData', middleware.checkToken, async (req, res) => {
+  try {
+    console.log(req.body)
+    const fashionCollection = db.collection('fashion')
+    await fashionCollection.add(req.body)
+    res.status(200).json({
+      msg: 'success',
     })
-    } catch (error) {
-        console.log(error)
-        res.send(error)
-    }
+  } catch (error) {
+    console.log(error)
+    res.send(error)
+  }
+})
+
+router.patch('/:fashionID', async (req, res) => {
+  try {
+    const { fashionID } = req.params
+    console.log(fashionID)
+    const updateFashionData = await db
+      .collection('fashion')
+      .doc(fashionID)
+      .update({ ...req.body })
+    res.status(200).json({
+      msg: 'success',
+    })
+  } catch (error) {
+    console.log(error)
+    res.send(error)
+  }
+})
+
+router.delete('/:fashionID', async (req, res) => {
+  try {
+    const { fashionID } = req.params
+    console.log(fashionID)
+    const updateFashionData = await db.collection('fashion').doc(fashionID).delete()
+    res.status(200).json({
+      msg: 'success',
+    })
+  } catch (error) {
+    console.log(error)
+    res.send(error)
+  }
 })
 
 module.exports = router
